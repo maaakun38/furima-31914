3d978a0d518f3815663d69f5e2a9d48562b84bdb
maaakun38 <75513347+maaakun38@users.noreply.github.com>
2021-03-07 14:19:14 +0900

    テスト

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
   #before_action :move_to_index, except: [:index, :show]
 
   #def index
+  #end
 
   #def move_to_index
   #  unless user_signed_in?
