fb8d24e3dccd18020f0b6a30ff9aba67a325c7e4
maaakun38 <75513347+maaakun38@users.noreply.github.com>
2021-03-16 12:08:42 +0900

    before_actionの導入

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_product, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @products = Product.order("created_at DESC")
@@ -19,18 +20,15 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit 
-    @product = Product.find(params[:id])
     unless @product.user_id == current_user.id
       redirect_to action: :index
     end
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to product_path(@product)
     else
@@ -38,9 +36,13 @@ class ProductsController < ApplicationController
     end
   end
 
-private
+  private
 
   def product_params
     params.require(:product).permit(:image, :title, :price, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :shipping_date_id).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
