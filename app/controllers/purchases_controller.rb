ba8ee95d121e5e14001bf8e177b123b06676df39
maaakun38 <75513347+maaakun38@users.noreply.github.com>
2021-03-20 18:34:59 +0900

    クリエイトアクションの設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,7 +2,25 @@ class PurchasesController < ApplicationController
   
   def index
       @product = Product.find(params[:product_id])
+      @purchase_buyer = PurchaseBuyer.new
   end
 
+  def create
+    @product = Product.find(params[:product_id])
+    @purchase_buyer = PurchaseBuyer.new(purchase_params)
+    if @purchase_buyer.valid? 
+      @purchase_buyer.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+
+  private
+
+  def purchase_params
+    params.permit(:postal_code, :prefecture_id, :municipality, :address, :building, :phone_number)
+  end
 
 end
